d9fa5a6bc01c9eb8c04ce7a99428f7e8a8093a1e
Geoff Hugo <gdhugo@wustl.edu>
2018-03-12 13:25:34 -0500

    add model display code

--- test.py
+++ test.py
@@ -12,6 +12,39 @@ from models.fcn8 import build_fcn8
 from metrics.metrics import cce_flatt, IoU
 from keras.optimizers import (RMSprop, Adam, SGD)
 
+def make_data(x_size, y_size, n_channels, n_samples):
+    """ makes testing and training data from scratch
+    returns x_train, y_train, x_test, y_test, x_valid, y_valid
+    """
+    in_shape = (x_size, y_size, n_channels)
+    x = np.zeros((n_samples, x_size, y_size, n_channels))
+    y = np.zeros((n_samples, x_size, y_size, n_channels))
+    n_classes = 2
+    void_class = [-1]
+
+    noise_data = (np.random.rand(n_samples, x_size, y_size, n_channels)-0.5) * 0.1 #[-0.05,0.05]
+    x_box_size = 10
+    y_box_size = 10
+    x_box_starts = np.floor( np.random.rand(n_samples) * (x_size - x_box_size)).astype('int')
+    y_box_starts = np.floor( np.random.rand(n_samples) * (y_size - y_box_size)).astype('int')
+    for sample in range(n_samples):
+
+        box_slice = np.zeros(in_shape)
+        box_slice[x_box_starts[sample]:x_box_starts[sample]+x_box_size,y_box_starts[sample]:y_box_starts[sample]+y_box_size,:] = 1.
+
+        y[sample,:,:,:] = box_slice
+        x[sample,:,:,:] = box_slice * 2. + noise_data[sample,:,:,:] + 0.05
+        x[sample,:,:,:] = x[sample,:,:,:] / 2.10 # normalize [0,1]
+
+
+    x_train = x[:125,:,:,:]
+    y_train = y[:125,:,:,:]
+    x_valid = x[126:150,:,:,:]
+    y_valid = y[126:150,:,:,:]
+    x_test = x[151:,:,:,:]
+    y_test = y[151:,:,:,:]
+    return x_train, y_train, x_test, y_test, x_valid, y_valid
+
 # Entry point of the script
 if __name__ == "__main__":
 
@@ -25,30 +58,7 @@ if __name__ == "__main__":
     y_size = 32
     n_channels = 1
     n_samples = 200
-    in_shape = (x_size, y_size, n_channels)
-    x = np.zeros((n_samples, x_size, y_size, n_channels))
-    y = np.zeros((n_samples, x_size, y_size, n_channels))
-    n_classes = 2
-    void_class = [-1]
-
-    noise_data = (np.random.rand(n_samples, x_size, y_size, n_channels)-0.5) * 0.1
-    x_box_size = 10
-    y_box_size = 10
-    x_box_starts = np.floor( np.random.rand(n_samples) * (x_size - x_box_size)).astype('int')
-    y_box_starts = np.floor( np.random.rand(n_samples) * (y_size - y_box_size)).astype('int')
-    for sample in range(n_samples):
-
-        box_slice = np.zeros(in_shape)
-        box_slice[x_box_starts[sample]:x_box_starts[sample]+x_box_size,y_box_starts[sample]:y_box_starts[sample]+y_box_size,:] = 1.
-
-        y[sample,:,:,:] = box_slice
-        x[sample,:,:,:] = box_slice * 2. + noise_data[sample,:,:,:]
-
-
-    x_train = x[:150,:,:,:]
-    y_train = y[:150,:,:,:]
-    x_test = x[151:,:,:,:]
-    y_test = y[151:,:,:,:]
+    (x_train, y_train, x_test, y_test, x_valid, y_valid) = make_data(x_size, y_size, n_channels, n_samples)
     print(x_train.shape)
     print(y_train.shape)
 
@@ -100,3 +110,13 @@ if __name__ == "__main__":
             plt.subplot(1,2,2)
             plt.imshow(y_pred[-1,:,:,0])
             plt.show()
+    else:
+        y_test = np.load('y_test.numpy.npy')
+        y_pred = np.load('y_pred.numpy.npy')
+        plt.subplot(1,3,1)
+        plt.imshow(y_test[0,:,:,0])
+        plt.subplot(1,3,2)
+        plt.imshow(y_pred[0,:,:,0])
+        plt.subplot(1,3,3)
+        plt.imshow(y_pred[0,:,:,1])
+        plt.show()
