e60fc614a55b36ea9c7e8e87acbdd4075a4829b7
Geoff Hugo <gdhugo@wustl.edu>
2018-03-12 16:25:17 -0500

    add modeling

--- test_full.py
+++ test_full.py
@@ -237,7 +237,7 @@ if __name__ == "__main__":
         cb = [EarlyStopping(monitor='train_loss', min_delta = 0.0001, patience=4)]
         model.fit_generator(train_generator,
                             nb_epoch=1000,
-                            steps_per_epoch=5
+                            steps_per_epoch=5,
                             callbacks=cb,
                             validation_data=valid_generator)
 
