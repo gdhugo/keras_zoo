6cca52536ec265f6e9955ebdbfbe8a3a0e6423cb
Geoff Hugo <gdhugo@wustl.edu>
2018-03-12 17:29:20 -0500

    add segmentation data generator

--- test_full.py
+++ test_full.py
@@ -19,7 +19,8 @@ from argparse import ArgumentParser
 from models.fcn8 import build_fcn8
 from metrics.metrics import cce_flatt, IoU
 from keras.optimizers import (RMSprop, Nadam, SGD)
-from keras.preprocessing.image import ImageDataGenerator
+#from keras.preprocessing.image import ImageDataGenerator
+from tools.seg_data_generator import SegDataGenerator
 from keras.callbacks import EarlyStopping
 
 def make_data(x_size, y_size, n_channels, n_samples):
@@ -71,6 +72,11 @@ def write_data(data, write_path, normalize=False):
         else:
             # find min/max
             scipy.misc.toimage(data[sample,:,:,0], cmin=0.0, cmax=1.0).save(file_name)
+
+def write_file_list(data, file_name):
+    with open(file_name, 'w') as f:
+        for sample in range( data.shape[0] ):
+            f.write("image{0}\n".format(sample))
 
 def write_config(x_size, y_size, write_path):
     """ write out config file for dataset
@@ -157,13 +163,13 @@ if __name__ == "__main__":
             print("Error:", sys.exc_info()[0])
             exit()
 
-    # subdirs. must be 'base/test/images/data' for flow_from_directory to work.
+    # subdirs.
     subdirs = ['test', 'train', 'valid']
     subsubdirs = ['images','masks']
 
     for subdir in subdirs:
         for subsubdir in subsubdirs:
-            new_path = join(data_dir, subdir, subsubdir, 'data')
+            new_path = join(data_dir, subdir, subsubdir)
             if not path.exists(new_path):
                 try:
                     makedirs(new_path)
@@ -172,12 +178,17 @@ if __name__ == "__main__":
                     exit()
 
     # write out data
-    write_data(x_train, join(data_dir, 'train', 'images', 'data'))
-    write_data(y_train, join(data_dir, 'train', 'masks', 'data'))
-    write_data(x_test, join(data_dir, 'test', 'images', 'data'))
-    write_data(y_test, join(data_dir, 'test', 'masks', 'data'))
-    write_data(x_valid, join(data_dir, 'valid', 'images', 'data'))
-    write_data(y_valid, join(data_dir, 'valid', 'masks', 'data'))
+    write_data(x_train, join(data_dir, 'train', 'images'))
+    write_data(y_train, join(data_dir, 'train', 'masks'))
+    write_data(x_test, join(data_dir, 'test', 'images'))
+    write_data(y_test, join(data_dir, 'test', 'masks'))
+    write_data(x_valid, join(data_dir, 'valid', 'images'))
+    write_data(y_valid, join(data_dir, 'valid', 'masks'))
+
+    # write out train.txt for SegDataGenerator
+    write_file_list(x_train, join(data_dir, 'train', 'train.txt'))
+    write_file_list(x_test, join(data_dir, 'test', 'test.txt'))
+    write_file_list(x_valid, join(data_dir, 'valid', 'valid.txt'))
 
     # write out config
     write_config(x_size, y_size, data_dir)
@@ -192,83 +203,124 @@ if __name__ == "__main__":
         model.compile(loss=loss, metrics=metrics, optimizer=opt)
 
         # data
-        train_gen_args = dict(samplewise_center=True,
-                            samplewise_std_normalization=True,
-                            width_shift_range=0.1,
-                            height_shift_range=0.1,
-                            zoom_range=0.2)
-        train_image_datagen = ImageDataGenerator(**train_gen_args)
-        train_mask_datagen = ImageDataGenerator(**train_gen_args)
+        train_datagen = SegDataGenerator(samplewise_center=True,
+                                     samplewise_std_normalization=True,
+                                     zoom_range=[0.1, 0.5],
+                                     zoom_maintain_shape=True,
+                                     crop_mode='random',
+                                     crop_size=target_size,
+                                     # pad_size=(505, 505),
+                                     rotation_range=0.,
+                                     shear_range=0,
+                                     horizontal_flip=True,
+                                     fill_mode='constant',
+                                     label_cval=255)
+        val_datagen = SegDataGenerator(samplewise_center=True,
+                                     samplewise_std_normalization=True)
 
-        seed = 1
-        train_image_generator = train_image_datagen.flow_from_directory(
-            join(data_dir,'train','images'),
-            class_mode=None,
-            seed=seed,
-            batch_size=16)
+        train_file_path = join(data_dir,'train','train.txt')
+        train_data_dir = join(data_dir,'train','images')
+        train_label_dir = join(data_dir,'train','masks')
+        val_file_path = join(data_dir,'valid','valid.txt')
+        val_data_dir = join(data_dir,'valid','images')
+        val_label_dir = join(data_dir,'valid','masks')
 
-        train_mask_generator = train_mask_datagen.flow_from_directory(
-            join(data_dir,'train','masks'),
-            class_mode=None,
-            seed=seed,
-            batch_size=16)
-        train_generator = zip(train_image_generator, train_mask_generator)
-
-        valid_gen_args = dict(samplewise_center=True,
-                              samplewise_std_normalization=True)
-        valid_image_datagen = ImageDataGenerator(**valid_gen_args)
-        valid_mask_datagen = ImageDataGenerator(**valid_gen_args)
-
-        seed = 1
-        valid_image_generator = valid_image_datagen.flow_from_directory(
-            join(data_dir,'valid','images'),
-            class_mode=None,
-            seed=seed,
-            batch_size=16)
-
-        valid_mask_generator = valid_mask_datagen.flow_from_directory(
-            join(data_dir,'valid','masks'),
-            class_mode=None,
-            seed=seed,
-            batch_size=16)
-        valid_generator = zip(valid_image_generator, valid_mask_generator)
-
-        # modeling
+        # train_gen_args = dict(samplewise_center=True,
+        #                     samplewise_std_normalization=True,
+        #                     width_shift_range=0.1,
+        #                     height_shift_range=0.1,
+        #                     zoom_range=0.2)
+        # train_image_datagen = ImageDataGenerator(**train_gen_args)
+        # train_mask_datagen = ImageDataGenerator(**train_gen_args)
+        #
+        # seed = 1
+        # train_image_generator = train_image_datagen.flow_from_directory(
+        #     join(data_dir,'train','images'),
+        #     class_mode=None,
+        #     seed=seed,
+        #     batch_size=16)
+        #
+        # train_mask_generator = train_mask_datagen.flow_from_directory(
+        #     join(data_dir,'train','masks'),
+        #     class_mode=None,
+        #     seed=seed,
+        #     batch_size=16)
+        # train_generator = zip(train_image_generator, train_mask_generator)
+        #
+        # valid_gen_args = dict(samplewise_center=True,
+        #                       samplewise_std_normalization=True)
+        # valid_image_datagen = ImageDataGenerator(**valid_gen_args)
+        # valid_mask_datagen = ImageDataGenerator(**valid_gen_args)
+        #
+        # seed = 1
+        # valid_image_generator = valid_image_datagen.flow_from_directory(
+        #     join(data_dir,'valid','images'),
+        #     class_mode=None,
+        #     seed=seed,
+        #     batch_size=16)
+        #
+        # valid_mask_generator = valid_mask_datagen.flow_from_directory(
+        #     join(data_dir,'valid','masks'),
+        #     class_mode=None,
+        #     seed=seed,
+        #     batch_size=16)
+        # valid_generator = zip(valid_image_generator, valid_mask_generator)
+        #
+        # # modeling
         cb = [EarlyStopping(monitor='train_loss', min_delta = 0.0001, patience=4)]
-        model.fit_generator(train_generator,
-                            epochs=1000,
+        # model.fit_generator(train_generator,
+        #                     epochs=1000,
+        #                     steps_per_epoch=5,
+        #                     callbacks=cb,
+        #                     validation_data=valid_generator,
+        #                     validation_steps=5)
+        #
+        model.fit_generator(generator=train_datagen.flow_from_directory(
+                                file_path=train_file_path,
+                                data_dir=train_data_dir, data_suffix='.png',
+                                label_dir=train_label_dir, label_suffix='.png',
+                                classes=2,
+                                target_size=(32,32), color_mode='grayscale',
+                                batch_size=16, shuffle=True,
+                                ignore_label=255,),
+                                # save_to_dir='Images/'),
                             steps_per_epoch=5,
+                            epochs=1000,
                             callbacks=cb,
-                            validation_data=valid_generator,
-                            validation_steps=5)
+                            validation_data=val_datagen.flow_from_directory(
+                                file_path=val_file_path, data_dir=val_data_dir, data_suffix='.png',
+                                label_dir=val_label_dir, label_suffix='.png',classes=2,
+                                target_size=(32,32), color_mode='grayscale',
+                                batch_size=16, shuffle=False),
+                            nb_val_samples = 5)
 
-        # testing
-        test_gen_args = dict(samplewise_center=True,
-                             samplewise_std_normalization=True)
-        test_image_datagen = ImageDataGenerator(**test_gen_args)
-        test_mask_datagen = ImageDataGenerator(**test_gen_args)
-
-        seed = 1
-        test_image_generator = test_image_datagen.flow_from_directory(
-            join(data_dir,'test','images'),
-            class_mode=None,
-            seed=seed,
-            batch_size=1)
-
-        filenames = test_image_generator.filenames
-        nb_samples = len(filenames)
-
-        test_mask_generator = test_mask_datagen.flow_from_directory(
-            join(data_dir,'test','masks'),
-            class_mode=None,
-            seed=seed)
-        test_generator = zip(test_image_generator, test_mask_generator)
-
-        score = model.evaluate_generator(test_generator, steps=nb_samples) #, batch_size=128)
-        y_pred = model.predict_generator(test_image_generator, steps=nb_samples)
-
-        print(score)
-        print(y_pred.shape)
+        # # testing
+        # test_gen_args = dict(samplewise_center=True,
+        #                      samplewise_std_normalization=True)
+        # test_image_datagen = ImageDataGenerator(**test_gen_args)
+        # test_mask_datagen = ImageDataGenerator(**test_gen_args)
+        #
+        # seed = 1
+        # test_image_generator = test_image_datagen.flow_from_directory(
+        #     join(data_dir,'test','images'),
+        #     class_mode=None,
+        #     seed=seed,
+        #     batch_size=1)
+        #
+        # filenames = test_image_generator.filenames
+        # nb_samples = len(filenames)
+        #
+        # test_mask_generator = test_mask_datagen.flow_from_directory(
+        #     join(data_dir,'test','masks'),
+        #     class_mode=None,
+        #     seed=seed)
+        # test_generator = zip(test_image_generator, test_mask_generator)
+        #
+        # score = model.evaluate_generator(test_generator, steps=nb_samples) #, batch_size=128)
+        # y_pred = model.predict_generator(test_image_generator, steps=nb_samples)
+        #
+        # print(score)
+        # print(y_pred.shape)
 
         # for sample in range(y_test.shape[0]):
         #     print('sample: ' + str(sample))
